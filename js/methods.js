843125452196c3365148f8cfc161cd66167f7297
Vladislav <nomadxfi@gmail.com>
2020-09-10 14:50:22 +0200

    added methods.js

---
+++ js/methods.js
@@ -0,0 +1,29 @@
+'use strict';
+
+// const str = 'tEst';
+// const arr = [1,2,3];
+
+// console.log(str.length);
+// console.log(str.toLocaleUpperCase());
+// console.log(str.toLocaleLowerCase());
+// console.log(arr.length);
+
+// let fruit = 'Some fruit lorem ipsulum';
+//
+// console.log(fruit.indexOf('lorem'));
+
+// const test = 'Test page';
+//
+// // console.log(test.slice(5));
+// console.log(test.substring(5));
+// console.log(test.substr(0, 4));
+
+let i = 129.2033434454;
+console.log(Math.round(i));
+
+const test = '12.9px';
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
+
+
